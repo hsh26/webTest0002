6c56e25e45858e9b6bf57b30fd2d423c6ec1180a
HwangSangHo <01052912015@arreo.com>
2019-11-20 16:10:40 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hsh <code>테스트</code> 테스트 테스트__.
+          hsh <code>테스트</code> 테스트 테스트.
         </p>
         <a
           className="App-link"
