03ab085c8cb3132474be6181bc9a45a4a4ced64d
HwangSangHo <01052912015@arreo.com>
2019-11-25 09:32:11 +0900

    기본세팅

---
+++ src/containers/TestPage.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+// import { connect } from 'react-redux';
+// import { injectIntl } from 'react-intl';
+
+// import './TestPage.scss';
+
+class TestPage extends React.Component {
+
+  render() {
+    return (
+        <div>test
+        </div>
+    );
+  }
+}
+
+export default TestPage
+// export default connect((state) => ({
+// }))(injectIntl(TestPage));
